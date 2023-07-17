d124d5adc7664d349ca0426707f650c205f201a8
Michael <m.anestopoulos24@gmail.com>
2023-07-17 18:04:10 -0400

    added some files for modules

--- src/load.js
+++ src/load.js
@@ -1,8 +1,11 @@
+import './styles.css';
+
 function load(){
 const content = document.getElementById('content');
 
 const title = document.createElement('h1');
 title.textContent = "Wow this is such a good restaurant";
+title.classList.add('test');
 
 content.appendChild(title);
 }
