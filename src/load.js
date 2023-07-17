1aa3460ff0ed77e52dd1720868e7cd71268eb051
Michael <m.anestopoulos24@gmail.com>
2023-07-17 19:27:51 -0400

    added some buttons in navbar

--- src/load.js
+++ src/load.js
@@ -2,11 +2,37 @@ import './styles.css';
 
 function load(){
 const content = document.getElementById('content');
+content.classList.add('container');
+
+const navbar = document.createElement('div');
+navbar.classList.add('navbar');
 
 const title = document.createElement('h1');
-title.textContent = "Wow this is such a good restaurant";
-title.classList.add('test');
+title.textContent = "YeeYeeRos";
+title.classList.add('title');
 
-content.appendChild(title);
+const navButtons = document.createElement('div');
+navButtons.classList.add('navButtonContainer');
+
+const homeBtn = document.createElement('button'); 
+homeBtn.id = 'homeBtn';
+homeBtn.innerHTML = 'Home';
+
+const menuBtn = document.createElement('button');
+menuBtn.id = 'menuBtn';
+menuBtn.innerHTML = 'Menu';
+
+const contactBtn = document.createElement('button');
+contactBtn.id = 'contactBtn';
+contactBtn.innerHTML = 'Contact Us';
+
+content.appendChild(navbar);
+navbar.appendChild(title);
+navbar.appendChild(navButtons);
+navButtons.appendChild(homeBtn);
+navButtons.appendChild(menuBtn);
+navButtons.appendChild(contactBtn);
+
+
 }
 export default load;
